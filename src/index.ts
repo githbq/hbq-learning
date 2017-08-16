981c1dc20d9b79da1b1841789ada99064a1ecf49
hbq <hbqhbq123@qq.com>
2017-08-17 00:20:49 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,46 @@
 /**
  * entry
  */
-console.log('hello typescript') 
+console.log('hello typescript')
+
+require('tslib')
+
+function f() {  //当调用C类method方法时其执行顺序是
+    console.log('f(): evaluated')   //-- 1
+    return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('f(): called')   //--4 
+        return {
+            value(...args: any[]) {
+                console.log('fffff', args)
+                descriptor.value.apply(this, args)
+                // return 'f'
+            }
+        }
+    }
+}
+
+function g() {
+    console.log('g(): evaluated')  //--2
+    return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('g(): called') //--3 
+        return {
+            value(...args: any[]) {
+                console.log('gggg', args)
+                descriptor.value.apply(this, args)
+                return 'g'
+            }
+        }
+    }
+}
+
+class C {
+    @f()
+    @g()
+    method(xxx): any {
+        console.log(111)
+        return 'method'
+    }  // --5
+}
+
+const c1 = new C()
+console.log(c1.method('xxx'))
